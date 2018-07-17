ab9ff28d9e921bd5cab542f7a1f6bf7ba6a6a226
Hector Lovo <lovohh@gmail.com>
2018-07-16 22:35:24 -0400

    Adding ShowAuthedDirective
    Used to display HTML based on whether
    the user is logged-in

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,6 +1,7 @@
 export * from './app-routing/app-routing.module';
 export * from './footer/footer.component';
 
+export * from './show-authed.directive';
 
 export * from './service/api.service';
 export * from './service/user.service';
