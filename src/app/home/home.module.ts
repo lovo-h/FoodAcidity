aa925d6b0def9eb4a7193cef353428b14c5ec70a
lovo-h <lovohh@gmail.com>
2018-10-02 06:36:19 -0400

    Attaching SubscribeModalComponent To HomeComponent
    Making SubscribeModalComponent available in the home
    page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { SendgridService, SharedModule } from '../shared';
 import { HomeComponent } from './home.component';
+import { SubscribeModalComponent } from './subscribe-modal/subscribe-modal.component';
+
 
 @NgModule({
   imports: [
     SharedModule
   ],
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SubscribeModalComponent
   ],
   providers: [
     SendgridService,
